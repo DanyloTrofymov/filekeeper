30949d624ede93fcfda032cd451420d546dc8076
Данила Трофимов <danya.trofimov@gmail.com>
2022-12-19 13:31:19 +0200

    style fix

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 import mongoose from 'mongoose';
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
-    try{
-        mongoose.connect(process.env.DB_URL)
+    try {
+        mongoose.connect(process.env.DB_URL);
 
         app.listen(PORT, () => {
-            console.log('Server started on port ', PORT)
-        })
-    } catch(e) { /* empty */ }
-}
+            console.log('Server started on port ', PORT);
+        });
+    } catch (e) {
+        /* empty */
+    }
+};
 
 start();
