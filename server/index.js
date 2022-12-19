7f5ce4f759c8dcde625efd9480954470b5648def
Данила Трофимов <danya.trofimov@gmail.com>
2022-12-19 11:37:38 +0200

    server init

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+import mongoose from 'mongoose';
+import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+const start = async () => {
+    try{
+        mongoose.connect(process.env.DB_URL)
+
+        app.listen(PORT, () => {
+            console.log('Server started on port ', PORT)
+        })
+    } catch(e) {
+
+    }
+}
+
+start();
